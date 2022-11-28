37338525a4b3a7935fbacaaa1cdedbd9c0660e9d
Iftee97 <tawsifmasrur@gmail.com>
2022-11-28 23:07:06 +0600

    refactors

--- stores/cartStore.js
+++ stores/cartStore.js
@@ -92,23 +92,5 @@ export const useCartStore = defineStore('cart', {
         })
       }
     },
-
-    async addProduct(product) {
-      const exists = this.cart.find((p) => p.id === product.id)
-
-      if (exists) {
-        this.incQuantity(product)
-      }
-
-      if (!exists) {
-        this.cart.push({ ...product, quantity: 1 })
-
-        // make post request
-        await $fetch('http://localhost:4000/cart', {
-          method: 'post',
-          body: JSON.stringify({ ...product, quantity: 1 })
-        })
-      }
-    }
   }
 })
